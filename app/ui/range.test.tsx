cbf4104a05b509eefdd7ed51d34e950467b9da63
Samuel <samuguemar@gmail.com>
2024-06-04 22:32:57 +0100

    test: add test for display a min value label

--- app/ui/range.test.tsx
+++ app/ui/range.test.tsx
@@ -1,11 +1,23 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
 import { Range } from './range'
 
 describe('Range component', () => {
+
+  afterEach(() => {
+    cleanup()
+  })
+
   it('should render the Range component succesfully', () => {
-    render(<Range />)
+    render(<Range min={1} />)
 
     expect(screen.getByTestId('range-container')).toBeDefined()
+  })
+
+  it('should display a label with the min value passed by params', () => {
+    const minValue = 1
+    render(<Range min={minValue} />)
+
+    expect(screen.getByText(`${minValue}€`)).toBeDefined()
   })
 })
