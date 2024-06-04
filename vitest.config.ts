e648fd5dee467b0e2492ae8bf8f33bdbe6a979ce
Samuel <samuguemar@gmail.com>
2024-06-04 23:01:40 +0100

    chore(vitest): add aliases configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
- 
+
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@/ui': resolve(__dirname, './app/ui'),
+      '@/lib': resolve(__dirname, './app/lib'),
+      '@': resolve(__dirname, './app'),
+    },
   },
 })
