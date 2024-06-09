eb1a59373325cac997a668c0b9aadbd14d5866c3
Samuel <samuguemar@gmail.com>
2024-06-09 23:01:59 +0100

    feat(home): add a heading and some links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import { Range } from '@/ui/range'
+import Link from 'next/link'
+
+import styles from '@/page.module.css'
 
 export default function Home() {
-  return <Range min={1} max={5} />
+  return (
+    <main>
+      <h2>Choose the Range Mode</h2>
+      <section className={styles.links}>
+        <Link className={styles.link} href="/exercise1">
+          Normal
+        </Link>
+        <Link className={styles.link} href="/exercise2">
+          Fixed values
+        </Link>
+      </section>
+    </main>
+  )
 }
