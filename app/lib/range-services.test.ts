e77de07ca94b8c3dad3cc3df72f60292dfa00393
Samuel <samuguemar@gmail.com>
2024-06-09 20:26:44 +0100

    test(services): add test for get fixed values range

--- app/lib/range-services.test.ts
+++ app/lib/range-services.test.ts
@@ -1,27 +1,51 @@
 import { describe, expect, it, vi } from 'vitest'
 
-import { BASE_API_URL, NORMAL_RANGE_ENDPOINT } from '@/lib/endpoints'
-import { getNormalRangeData } from '@/lib/range-services'
-import { NormalRangeData } from '@/lib/definitions'
+import {
+  BASE_API_URL,
+  FIXED_VALUES_RANGE_ENDPOINT,
+  NORMAL_RANGE_ENDPOINT,
+} from '@/lib/endpoints'
+import {
+  getFixedValuesRangeData,
+  getNormalRangeData,
+} from '@/lib/range-services'
+import { FixedValuesRangeData, NormalRangeData } from '@/lib/definitions'
 
 const fetchSpy = vi.spyOn(global, 'fetch')
 
-const mockData: NormalRangeData = {
-  min: 1,
-  max: 100,
-}
-
 describe('Range services', () => {
-  it('should get the data from the API successfully', async () => {
-    fetchSpy.mockResolvedValueOnce({
-      json: async () => ({ ...mockData }),
-    } as Response)
+  describe('getNormalRangeData', () => {
+    const mockData: NormalRangeData = {
+      min: 1,
+      max: 100,
+    }
+    it('should get the data from the API successfully', async () => {
+      fetchSpy.mockResolvedValueOnce({
+        json: async () => ({ ...mockData }),
+      } as Response)
 
-    const rangeData = await getNormalRangeData()
+      const rangeData = await getNormalRangeData()
+      const endpoint = `${BASE_API_URL}${NORMAL_RANGE_ENDPOINT}`
 
-    const endpoint = `${BASE_API_URL}${NORMAL_RANGE_ENDPOINT}`
-    expect(fetchSpy).toHaveBeenCalledWith(endpoint)
+      expect(fetchSpy).toHaveBeenCalledWith(endpoint)
+      expect(rangeData).toEqual(mockData)
+    })
+  })
 
-    expect(rangeData).toEqual(mockData)
+  describe('getFixedValuesRangeData', () => {
+    const mockData: FixedValuesRangeData = {
+      valueRange: [1, 2, 3, 4],
+    }
+    it('should get the data from the API successfully', async () => {
+      fetchSpy.mockResolvedValueOnce({
+        json: async () => ({ ...mockData }),
+      } as Response)
+
+      const rangeData = await getFixedValuesRangeData()
+      const endpoint = `${BASE_API_URL}${FIXED_VALUES_RANGE_ENDPOINT}`
+
+      expect(fetchSpy).toHaveBeenCalledWith(endpoint)
+      expect(rangeData).toEqual(mockData)
+    })
   })
 })
