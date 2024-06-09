4315d1d042b1147f9aa8c7da482c34118c80ca6f
Samuel <samuguemar@gmail.com>
2024-06-09 12:18:32 +0100

    test: add test to get range data from API

--- app/exercise1/page.test.tsx
+++ app/exercise1/page.test.tsx
@@ -1,7 +1,18 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 
 import NormalRange from '@/exercise1/page'
+
+vi.mock('@/lib/range-services', async (importOriginal) => {
+  const mod = await importOriginal<typeof import('@/lib/range-services')>()
+  return {
+    ...mod,
+    getNormalRangeData: vi.fn().mockResolvedValue({
+      min: 1,
+      max: 100,
+    }),
+  }
+})
 
 describe('Exercise 1', () => {
   it('should render the exercise 1 page with a range component', () => {
@@ -9,5 +20,12 @@ describe('Exercise 1', () => {
 
     expect(screen.getByRole('heading', { name: 'Normal Range', level: 2 }))
     expect(screen.getByTestId('range-container'))
+  })
+
+  it('should initialize the range values from the API call', () => {
+    render(<NormalRange />)
+
+    expect(screen.findByText('100€')).toBeDefined()
+    expect(screen.findByText('1€')).toBeDefined()
   })
 })
