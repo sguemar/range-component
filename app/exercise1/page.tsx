085788daba9988dbd53b5a08ed0a664bbf4ae8d4
Samuel <samuguemar@gmail.com>
2024-06-09 12:19:35 +0100

    feat(normal-range): get data from the API

--- app/exercise1/page.tsx
+++ app/exercise1/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { getNormalRangeData } from '@/lib/range-services'
+import { NormalRangeResponse } from '@/lib/definitions'
 import { Range } from '@/ui/range'
 
 export default function NormalRange() {
+  const [data, setData] = useState<NormalRangeResponse>({
+    max: 0,
+    min: 0,
+  })
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getNormalRangeData()
+      setData(data)
+    }
+    getData()
+  }, [])
+
   return (
     <>
       <h2>Normal Range</h2>
-      <Range max={70.99} min={7.99} />
+      <Range max={data.max} min={data.min} />
     </>
   )
 }
