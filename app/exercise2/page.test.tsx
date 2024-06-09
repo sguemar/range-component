c8e3fa9281b74d9aa4504497ebdba238c39d1f2e
Samuel <samuguemar@gmail.com>
2024-06-09 20:31:02 +0100

    test: add tests for exercise 2

---
+++ app/exercise2/page.test.tsx
@@ -0,0 +1,55 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+
+import FixedValuesRange from '@/exercise2/page'
+
+const { getFixedValuesRangeData } = vi.hoisted(() => {
+  return {
+    getFixedValuesRangeData: vi.fn().mockResolvedValue({
+      valueRange: [1, 2, 3, 4],
+    }),
+  }
+})
+
+vi.mock('@/lib/range-services', async (importOriginal) => {
+  const mod = await importOriginal<typeof import('@/lib/range-services')>()
+  return {
+    ...mod,
+    getFixedValuesRangeData,
+  }
+})
+
+describe('Exercise 2', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('should render the exercise 2 page with a range component', () => {
+    render(<FixedValuesRange />)
+
+    const rangeComponent = screen.findByTestId('range-container')
+
+    expect(
+      screen.getByRole('heading', { name: 'Fixed values Range', level: 2 }),
+    )
+    expect(rangeComponent).toBeDefined()
+  })
+
+  it('should initialize the range values from the API call', async () => {
+    const mockData = {
+      valueRange: [5, 7.5, 11, 24],
+    }
+    getFixedValuesRangeData.mockResolvedValue(mockData)
+
+    render(<FixedValuesRange />)
+
+    const minValue = await screen.findByText(`${mockData.valueRange[0]}€`)
+    const numberOfFixedValues = mockData.valueRange.length
+    const maxValue = await screen.findByText(
+      `${mockData.valueRange[numberOfFixedValues - 1]}€`,
+    )
+
+    expect(minValue).toBeDefined()
+    expect(maxValue).toBeDefined()
+  })
+})
