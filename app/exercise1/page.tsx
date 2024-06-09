c81d8b49cc22e955f17c78b3a837871d6d3fdae2
Samuel <samuguemar@gmail.com>
2024-06-09 10:30:28 +0100

    feat: add exercise 1 page

---
+++ app/exercise1/page.tsx
@@ -0,0 +1,10 @@
+import { Range } from '@/ui/range'
+
+export default function NormalRange() {
+  return (
+    <>
+      <h2>Normal Range</h2>
+      <Range max={70.99} min={7.99} />
+    </>
+  )
+}
