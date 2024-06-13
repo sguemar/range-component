e183642f3bed8fa5f54fc55243b158552b21487d
Samuel <samuguemar@gmail.com>
2024-06-13 14:16:26 +0100

    refactor(exercise1): use the api hook
    Use the API hook to handle the loading and error states

--- app/exercise1/page.tsx
+++ app/exercise1/page.tsx
@@ -5,27 +5,35 @@ import { useEffect, useState } from 'react'
 import { getNormalRangeData } from '@/lib/range-services'
 import { NormalRangeData } from '@/lib/definitions'
 import { Range } from '@/ui/range'
+import { useApi } from '@/lib/hooks/useApi'
 
 export default function NormalRange() {
   const [data, setData] = useState<NormalRangeData>({
     max: 0,
     min: 0,
   })
-  const [isLoading, setIsLoading] = useState(true)
+  const { apiCallHandler, isLoading, error } = useApi()
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getNormalRangeData()
-      setIsLoading(false)
+  const getData = async () => {
+    const data = await apiCallHandler<NormalRangeData>({
+      service: getNormalRangeData,
+    })
+    if (!error) {
       setData(data)
     }
+  }
+
+  useEffect(() => {
     getData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  if (isLoading) return <p>Loading...</p>
 
   return (
     <>
       <h2>Normal Range</h2>
-      {isLoading ? <p>Loading...</p> : <Range max={data.max} min={data.min} />}
+      {error ? <p>{error}</p> : <Range max={data.max} min={data.min} />}
     </>
   )
 }
